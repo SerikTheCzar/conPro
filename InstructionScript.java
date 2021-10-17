60ae73d2833d72f60de2512e55180c6d88f90725
CasuallyCritical <onetruejdyt@gmail.com>
2021-10-17 13:24:51 -0400

    Update InstructionScript.java
    Added core logic

--- InstructionScript.java
+++ InstructionScript.java
@@ -1,1 +1,25 @@
 //InstructionScript
+/*
+/ Created by: Jonathan Gerraughty
+*/
+
+
+public class InstructionScript {
+
+	public String name = "";
+	public int PCCount = 0;
+	
+	public void Cycle() {
+		PCCount --;
+	}
+	
+	public InstructionScript(String name, int PCCount) {
+		this.name = name;
+		this.PCCount = PCCount;
+	}
+	
+	public String toString() {
+		return name + ": " + PCCount;
+	}
+	
+}
